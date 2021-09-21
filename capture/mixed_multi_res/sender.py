f3df8db1ec8eb9bec9a523851bd43ef523afb285
vithurson <vithurson@gmail.com>
2021-09-21 06:00:35 +0100

    latest update

--- capture/mixed_multi_res/sender.py
+++ capture/mixed_multi_res/sender.py
@@ -1,57 +1,54 @@
-import os
+import os,glob,sys
 from time import sleep
+import numpy as np
 import socket
-from time import time
+import time
 import psutil
-file1='test.avi'
-file2='test2.avi'
-dur = os.path.getmtime(file1)
-dur2 = os.path.getmtime(file2)
-count=0
-las=0
-las2=0
+import cv2
 TCP_PORT = 5003
-TCP_IP = '192.168.18.19'
+TCP_IP = '192.168.147.148'
 n=0
 sock = socket.socket()
 sock.connect((TCP_IP, TCP_PORT))
-def send_chuncks(data,lent):
+def send_chunks(data,lent):
         if(lent>0):
             size = str(len(data)).ljust(16).encode('utf-8')
             sock.send(size)
             sock.send(data)
-lenss = 0
-started=0
-while True:
-    t = time()
-    if (dur != os.path.getmtime(file1)):
-        started=1
-        dur = os.path.getmtime(file1)
-        f = open(file1)
-        f.seek(las)
-        l = b'1'+f.read()
-        las=f.tell()
-        send_chuncks(l,len(l)) 
-        lenss+=len(l)
-        f.close()
-    if (dur2 != os.path.getmtime(file2)):
-        started=1
-        dur2 = os.path.getmtime(file2)
-        f = open(file2)
-        f.seek(las2)
-        l = b'2'+f.read()
-        las2=f.tell()
-        send_chuncks(l,len(l)) 
-        lenss+=len(l)
-        f.close()
-    if(started and os.path.exists("running.re")):
-        pass
-    elif(started):
-        print("not running")
-        sock.close()
-        break
-    delay =0.125 - (time()-t)
-    if(delay<0):
-        print(delay)
-        delay = 0
-    sleep(delay)
+path=sys.argv[1]
+os.chdir(path)
+dat=[]
+for file in glob.glob("*.avi"):
+    cap= open(file,'rb') 
+    bytes=b''
+    while True:
+        bytes += cap.read(1024)
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        if(a!=-1 and b!=-1):
+            jpg = bytes[a:b+2] 
+            bytes = bytes[b+2:]
+            dat.append(jpg)
+            break
+true= True
+while true:
+    t=time.time()
+    for data in dat:
+        send_chunks(data,len(data))
+    d= time.time()-t
+    if(d>0.125):
+        print("neg",d)
+        d=0.125 
+    sleep(0.125-d)
+#while True:;
+#    t = time()
+#    if (dur != os.path.getmtime(file1)):
+#        started=1
+#        dur = os.path.getmtime(file1)
+#        f = open(file1)
+#        f.seek(las)
+#        l = b'1'+f.read()
+#        las=f.tell()
+#        send_chuncks(l,len(l)) 
+#        lenss+=len(l)
+#        f.close()
