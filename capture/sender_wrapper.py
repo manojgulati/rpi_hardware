7daf8258a1ab087e4676675f8c50e29a04f1ef18
vithurson <vithurson@github.com>
2021-10-29 01:00:13 +0800

    writing masterscript for client

--- capture/sender_wrapper.py
+++ capture/sender_wrapper.py
@@ -1,5 +1,19 @@
-import os
+import os,sys
 from time import sleep
-while 1:
-    os.system("python sender.py")
-    sleep(0.5)
+try:
+    client_no = int(sys.argv[1])
+except:
+    try:
+        client_no = int(input("enter the client number "))
+    except:
+        sys.exit(0)
+try:
+    TCP_IP = sys.argv[2]
+    while 1:
+        os.system("python sender.py "+str(client_no)+" "+TCP_IP)
+        sleep(0.5)
+except:
+    while 1:
+        os.system("python sender.py "+str(client_no))
+        sleep(0.5)
+
