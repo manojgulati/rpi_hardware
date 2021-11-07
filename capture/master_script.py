629d753abe5e5e40bfa6c19b78332d42f6998e49
vithurson@gmail.com <vithurson@gmail.com>
2021-11-07 16:43:02 +0800

    upload script update

--- capture/master_script.py
+++ capture/master_script.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 import os,sys
-os.system("./run.sh 4000 400")
+os.system("./run.sh "+sys.argv[4] +" 400")
 if(len(sys.argv)<4):
-    print("usage ./master_script.py <client_id> <percentage_shared_region> <number_images_to_capture> <server_ip>(optional)")
+    print("usage ./master_script.py <client_id> <percentage_shared_region> <number_images_to_capture> <exposure> <server_ip>(optional)")
     sys.exit(1)
 os.system("rm -rf con_done")
-if(len(sys.argv)==4):
+if(len(sys.argv)==5):
     os.system("python sender_wrapper.py "+ " "+sys.argv[1]+" &")
 else:
-    os.system("python sender_wrapper.py "+ " "+sys.argv[1]+ " "+sys.argv[4]+" &")
+    os.system("python sender_wrapper.py "+ " "+sys.argv[1]+ " "+sys.argv[5]+" &")
 print("waiting to establish connection")
 try:
     while(not(os.path.exists("con_done"))):
